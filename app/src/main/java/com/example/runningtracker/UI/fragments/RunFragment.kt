fd54239953649f496d31d65b0eaeef832553a332
AlexDragon182 <alex_182_sp@outlook.com>
2023-02-27 15:42:45 -0600

    Running Tracker For learning Dagger Hilt and Google

--- app/src/main/java/com/example/runningtracker/UI/fragments/RunFragment.kt
+++ app/src/main/java/com/example/runningtracker/UI/fragments/RunFragment.kt
@@ -1,11 +1,26 @@
 package com.example.runningtracker.UI.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.runningtracker.R
+import com.example.runningtracker.databinding.FragmentRunBinding
 
 
 class RunFragment : Fragment(R.layout.fragment_run) {
 
+ private var _binding: FragmentRunBinding? = null
+ private val binding get() = _binding!!
 
+ override fun onCreateView(
+  inflater: LayoutInflater,
+  container: ViewGroup?,
+  savedInstanceState: Bundle?
+ ): View? {
+  _binding = FragmentRunBinding.inflate(inflater,container,false)
+  return binding.root
+ }
 
 }
