7c626735aa75e05103a18b49261dfff9fae95ef6
AlexDragon182 <alex_182_sp@outlook.com>
2023-03-27 15:19:04 -0600

    Running Tracker For learning Dagger Hilt and Google

--- app/src/main/java/com/example/runningtracker/UI/fragments/RunFragment.kt
+++ app/src/main/java/com/example/runningtracker/UI/fragments/RunFragment.kt
@@ -1,5 +1,7 @@
 package com.example.runningtracker.UI.fragments
 
+import android.Manifest
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,14 +9,19 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import com.example.runningtracker.Other.Constants.REQUEST_CODE_LOCATION_PERMISSION
+import com.example.runningtracker.Other.TrackingUtility
 import com.example.runningtracker.R
 import com.example.runningtracker.UI.ViewModels.MainViewModel
 import com.example.runningtracker.databinding.FragmentRunBinding
 import dagger.hilt.android.AndroidEntryPoint
+import pub.devrel.easypermissions.AppSettingsDialog
+import pub.devrel.easypermissions.EasyPermissions
 
 
 @AndroidEntryPoint // to annotate view models with Dagger Hilt
-class RunFragment : Fragment(R.layout.fragment_run) {
+
+class RunFragment : Fragment(R.layout.fragment_run),EasyPermissions.PermissionCallbacks {
 
  private var _binding: FragmentRunBinding? = null
  private val binding get() = _binding!!
@@ -30,6 +37,7 @@ class RunFragment : Fragment(R.layout.fragment_run) {
  }
 
  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+  requestPersmissions()
   super.onViewCreated(view, savedInstanceState)
 
   binding.fab.setOnClickListener{
@@ -37,4 +45,50 @@ class RunFragment : Fragment(R.layout.fragment_run) {
   }
  }
 
-}
+ private fun requestPersmissions () {
+  if(TrackingUtility.hasLocationPermissions(requireContext())){
+   return
+  }
+  if(Build.VERSION.SDK_INT < Build.VERSION_CODES.Q){
+   EasyPermissions.requestPermissions(
+    this ,"You need to accept this permissions to use this app",
+    REQUEST_CODE_LOCATION_PERMISSION,
+    Manifest.permission.ACCESS_COARSE_LOCATION,
+    Manifest.permission.ACCESS_FINE_LOCATION,
+   )
+  }else {
+   EasyPermissions.requestPermissions(
+    this ,"You need to accept this permissions to use this app",
+    REQUEST_CODE_LOCATION_PERMISSION,
+    Manifest.permission.ACCESS_COARSE_LOCATION,
+    Manifest.permission.ACCESS_FINE_LOCATION,
+    Manifest.permission.ACCESS_BACKGROUND_LOCATION,
+   )
+  }
+ }
+
+ override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {
+  if(EasyPermissions.somePermissionPermanentlyDenied(this,perms)) {
+   AppSettingsDialog.Builder(this).build().show()
+  }else{
+    requestPersmissions()
+   }
+  }
+ override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
+
+ }
+
+ override fun onRequestPermissionsResult(
+  requestCode: Int,
+  permissions: Array<out String>,
+  grantResults: IntArray
+ ) {
+  super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+  EasyPermissions.onRequestPermissionsResult(requestCode,permissions,grantResults,this)
+
+ }
+
+ }
+
+
+
--- app/src/main/java/com/example/runningtracker/UI/fragments/TrackingFragment.kt
+++ app/src/main/java/com/example/runningtracker/UI/fragments/TrackingFragment.kt
@@ -5,12 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.room.util.query
 import com.example.runningtracker.R
 import com.example.runningtracker.databinding.FragmentTrackingBinding
+import com.google.android.gms.maps.GoogleMap
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class TrackingFragment:Fragment(R.layout.fragment_tracking) {
     private var _binding: FragmentTrackingBinding? = null
     private val binding get() = _binding!!
+    private var map : GoogleMap? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,4 +25,45 @@ class TrackingFragment:Fragment(R.layout.fragment_tracking) {
         _binding = FragmentTrackingBinding.inflate(inflater,container,false)
         return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.mapView.onCreate(savedInstanceState)
+
+        binding.mapView.getMapAsync{
+            map = it
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        binding.mapView?.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.mapView?.onResume()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        binding.mapView?.onStop()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        binding.mapView?.onPause()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        binding.mapView?.onLowMemory()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        binding.mapView.onSaveInstanceState(outState)
+    }
 }
+
