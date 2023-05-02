74a671bee8fbdf0f89529bab843eaa48638261db
alex_ <alex_182_sp@outlook.com>
2023-05-02 09:04:12 -0700

    Files for Running Tracker , Learning Dagger Hilt and Google Services

--- app/src/main/java/com/example/runningtracker/database/Run.kt
+++ app/src/main/java/com/example/runningtracker/database/Run.kt
@@ -3,14 +3,14 @@ package com.example.runningtracker.database
 import android.graphics.Bitmap
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-
+//an entitiy is nothing more than our database
 @Entity(tableName = "running_table") //this is an entity for our room data base (a table)
 data class Run(
     var img: Bitmap? = null,//preview image
     var timestamp: Long = 0L,//the amount of milliseconds, Date converted to miliseconds, its more easy to sort than days
-    var avgSpeedInKMH: Float = 0f,//
-    var distanceInMeters: Int = 0,
-    var timeInMillis: Long = 0,// describes how long our run is
+    var avgSpeedInKMH: Float = 0f,// average speed in kilometers
+    var distanceInMeters: Int = 0, // distance in meters.
+    var timeInMillis: Long = 0L,// describes how long our run is
     var caloriesBurned: Int = 0 // calories burnt
 ) {
     @PrimaryKey(autoGenerate = true)
